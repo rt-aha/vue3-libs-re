84d911de3aa6700ff4ad46bafaf8a36fd3d85630
tsai_ray <silentice1534@gmail.com>
2022-09-24 21:48:43 +0800

    feat: tooltip

---
+++ src/components/feedback/ReTooltip.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="re-tooltip">
+    <slot></slot>
+    <p
+      class="re-tooltip__desc"
+      :class="[`re-tooltip__desc--${placement}`, `re-tooltip__desc--cus-type--${cusPlacementType}`]"
+    >
+      {{ label }}
+    </p>
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ReTooltip',
+  props: {
+    label: {
+      type: String,
+      default: 'tooltip',
+    },
+    placement: {
+      type: String,
+      default: 'center',
+    },
+    cusPlacementType: {
+      type: String,
+      default: '',
+    },
+  },
+});
+</script>
+<style lang="scss" scoped>
+.re-tooltip {
+  position: relative;
+
+  &:hover {
+    .re-tooltip__desc {
+      display: inline-block;
+    }
+  }
+
+  &__desc {
+    background-color: rgba($c-black, 0.8);
+    border-radius: 4px;
+    @include padding(4px 6px);
+    display: none;
+    white-space: nowrap;
+    @include font-style($c-white, 12);
+
+    &--left {
+      @include position(tl, -100%, 0);
+    }
+
+    &--center {
+      @include position(tl, 0%, 50%);
+      transform: translate(-50%, calc(-100% - 5px));
+    }
+
+    &--cus-type {
+      @include position(tl, -100%, 50%);
+      transform: translateX(-50%);
+
+      &--order-name {
+        @include position(tl, -25px, 50%);
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/feedback/ReTooltip.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="v-tooltip">
+    <dev-section title="基本使用">
+      <re-tooltip label="這個按鈕說明">
+        <re-button size="small"> 小按鈕 </re-button>
+      </re-tooltip>
+    </dev-section>
+    <dev-section title="基本使用">
+      <re-tooltip label="新增">
+        <img class="demo-icon" src="@/assets/icon/add.svg" />
+      </re-tooltip>
+      <re-tooltip label="編輯">
+        <img class="demo-icon" src="@/assets/icon/edit.svg" />
+      </re-tooltip>
+      <re-tooltip label="刪除">
+        <img class="demo-icon" src="@/assets/icon/delete.svg" />
+      </re-tooltip>
+    </dev-section>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import ReTooltip from '@/components/feedback/ReTooltip.vue';
+import ReButton from '@/components/common/ReButton.vue';
+
+export default defineComponent({
+  name: 'ViewTooltip',
+  components: {
+    ReTooltip,
+    ReButton,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.demo-icon {
+  cursor: pointer;
+}
+</style>
